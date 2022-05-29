019051d71b6dae59b563b355808cad78dc994cb7
Mohd Shahnawaz <32464723+shahnawaz8@users.noreply.github.com>
2022-05-29 22:59:11 +0530

    completed

--- src/Components/Ballon.jsx
+++ src/Components/Ballon.jsx
@@ -17,7 +17,7 @@ export const Ballons = ()=>{
     }
     return(
         <>
-            <h1>Sygenta Assignment</h1>
+            <h1>Ballons Assignment</h1>
             <div className="mainDiv">
                 <div className='mainDivLeft'>
 
@@ -39,18 +39,11 @@ export const Ballons = ()=>{
                         {!three?<div style={{'backgroundColor':'#6a00ff'}} onClick={()=>three?setThree(false):setThree(true)} className="color color3">3</div>:""}
                         {!four?<div style={{"backgroundColor":'#d9e8fb'}} onClick={()=>four?setFour(false):setFour(true)} className="color color4">4</div>:""}
                         {!five?<div style={{"backgroundColor":'#e2d5e7'}} className="color color5" onClick={()=>five?setFive(false):setFive(true)}>5</div>:""}
-                        
+
                 </div>
                 <div className='mainDivRight'>
 
-                    <select className='select-tag' onChange={(e)=>setSelet(e.target.value)}>
-                        <option value="0">Selet Circle</option>
-                        <option value="1" >1</option>
-                        <option value="2" >2</option>
-                        <option value="3" >3</option>
-                        <option value="4" >4</option>
-                        <option value="5" >5</option>
-                    </select>
+                    <input className='text-box' type="number" value={selected} onChange={(e)=>setSelet(e.target.value)}/>
                     <button onClick={()=>handleNumber(selected)}>shoot</button>
       
                 </div>
